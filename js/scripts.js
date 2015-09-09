6006be84454921c725fb5ad30fdee861ff5eded4
Alex Altieri & Vaughn Dice <student@epicodus.com>
2015-09-09 16:02:50 -0700

    add calcExecTime helper method to clock performance of given function

--- js/scripts.js
+++ js/scripts.js
@@ -26,23 +26,29 @@ var eratosthenize = function(number) {
   return numbers;
 }
 
+var calcExecTime = function(arg, fcn) {
+  var start = performance.now();
+  var result = fcn(arg);
+  var end = performance.now();
+  return {
+    result: result,
+    execTime: end - start
+  };
+}
 
 $(document).ready(function() {
   $("form#input_form").submit(function(event) {
     var inputNumber  = $("input#number").val();
 
-
     // get execution time for Sieve of Eratosthenes
-    var start = performance.now();
-    var ePrimes = eratosthenize(inputNumber);
-    var end = performance.now();
-    var eTime = end - start;
+    var eExec = calcExecTime(inputNumber, eratosthenize);
+    var ePrimes = eExec.result;
+    var eTime = eExec.execTime;
 
     // get execution time for Get Sieved
-    start = performance.now();
-    var gPrimes = getSieved(inputNumber);
-    end = performance.now();
-    var gTime = end - start;
+    var gExec = calcExecTime(inputNumber, getSieved);
+    var gPrimes = gExec.result;
+    var gTime = gExec.execTime;
 
     if (gTime < eTime) {
       $('#winning-sieve').text('Get Sieved');
